c94ed8f806b1025356ba5e09b1bc9cef94c0f5ba
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-06-04 23:00:02 -0300

    I don't like requires everywhere...

--- dotfiles/xdg_config_home/nvim/lua/eruizc/lsp.lua
+++ dotfiles/xdg_config_home/nvim/lua/eruizc/lsp.lua
@@ -1,5 +1,6 @@
 local lspconfig = require("lspconfig")
 local utils = require("eruizc.utils")
+local lsputil = require("lsputil.codeAction")
 
 local checkstyle = {
   lintCommand = "checkstyle -c checkstyle.xml ${INPUT}",
@@ -30,7 +31,7 @@ lspconfig.gopls.setup{}
 lspconfig.jdtls.setup{
   cmd = { "jdtls" },
   root_dir = function(f)
-    return require'lspconfig'.util.root_pattern('pom.xml', 'gradle.build', '.git')(f) or vim.fn.getcwd()
+    return lspconfig.util.root_pattern('pom.xml', 'gradle.build', '.git')(f) or vim.fn.getcwd()
   end
 }
 lspconfig.jsonls.setup{}
@@ -58,7 +59,7 @@ lspconfig.tsserver.setup{}
 lspconfig.vimls.setup{}
 lspconfig.yamlls.setup{}
 
-vim.lsp.handlers["textDocument/codeAction"] = require("lsputil.codeAction").code_action_handler
+vim.lsp.handlers["textDocument/codeAction"] = lsputil.code_action_handler
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
     signs = true,
