403f096a09fcca7a269d939c988a8568baf4883d
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-06-23 20:02:22 -0300

    Add font preloading

--- website/pages/index.jsx
+++ website/pages/index.jsx
@@ -19,6 +19,8 @@ function Header() {
       <title>Emiliano Ruiz Carletti</title>
       <meta name="description" content="Software developer" />
       <link rel="icon" href="/favicon.ico" />
+      <link href="/fonts/CostaRica/CostaRica-Regular.woff" as="font" rel="preload" crossOrigin="" />
+      <link href="/fonts/Montserrat/Montserrat-Regular.woff" as="font" rel="preload" crossOrigin="" />
     </Head>
   );
 }
