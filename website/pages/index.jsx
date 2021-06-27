8f829d329e8d55677dd15ef494335b6a336f95b5
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-06-27 17:43:11 -0300

    Improve SEO

--- website/pages/index.jsx
+++ website/pages/index.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Center from '../utils/center'
 import BottomCenter from '../utils/bottomCenter'
 import Banner from '../utils/banner'
+import Metatags from '../utils/metatags'
 import { Github, Twitter, Linkedin } from '../utils/icons'
 
 export default function Home() {
@@ -23,11 +24,13 @@ export default function Home() {
 function Header() {
   return (
     <Head>
-      <title>Emiliano Ruiz Carletti</title>
-      <meta name="description" content="Software developer" />
+      <title>Emiliano Ruiz Carletti - Software Developer</title>
       <link rel="icon" href="/favicon.png" />
       <link href="/fonts/CostaRica/CostaRica-Regular.woff" as="font" rel="preload" crossOrigin="" />
       <link href="/fonts/Montserrat/Montserrat-Regular.woff" as="font" rel="preload" crossOrigin="" />
+      <Metatags.Primary />
+      <Metatags.OpenGraph />
+      <Metatags.Twitter />
     </Head>
   );
 }
