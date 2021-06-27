0927526bb91843e9bc741949d78433c4e4390b5e
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-06-27 00:22:19 -0300

    Responsive icons

--- website/utils/icons.jsx
+++ website/utils/icons.jsx
@@ -1,20 +1,18 @@
 import GithubIcon from '../public/icons/brand-logos-1/svg/github-1288242.svg'
 import TwitterIcon from '../public/icons/brand-logos-1/svg/twitter-1239448.svg'
 import LinkedinIcon from '../public/icons/brand-logos-1/svg/linkedin-1239440.svg'
-import PropTypes from 'prop-types'
 
 const defaultProps = {
-  width: 128,
-  height: 128,
-  color: "#35BF5C"
+  width: 'calc(70px + 4vw)',
+  height: 'calc(70px + 4vw)',
+  color: '#35BF5C'
 }
-
 
 Github.defaultProps = defaultProps;
 export function Github({ width, height, color }) {
   return(
     <a href="https://github.com/eruizc-dev">
-      <GithubIcon width={width} height={height} style={{fill: color}} />
+        <GithubIcon style={{height: height, width: width, fill: color}} />
     </a>
   )
 }
