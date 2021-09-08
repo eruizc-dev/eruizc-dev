ffc74e518eb0701cba4627da4570445bc96522e4
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-09-07 23:22:11 -0300

    Add cmp mappings

--- dotfiles/xdg_config_home/nvim/lua/eruizc/completion.lua
+++ dotfiles/xdg_config_home/nvim/lua/eruizc/completion.lua
@@ -2,6 +2,13 @@ local cmp = require'cmp'
 local tabnine = require('cmp_tabnine.config')
 
 cmp.setup({
+  mapping = {
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-h>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-l>'] = cmp.mapping.scroll_docs(4),
+    ['<CR>'] = cmp.mapping.complete() and cmp.mapping.close()
+  },
   sources = {
     { name = 'buffer' },
     { name = 'nvim_lsp' },
