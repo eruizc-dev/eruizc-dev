70df781f418a85a02f619ae05789119b153fad07
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-01-04 16:57:55 -0300

    Add wezterm configuration

---
+++ dotfiles/xdg_config_home/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require('wezterm')
+
+return {
+  color_scheme = 'Brogrammer',
+  font = wezterm.font('TerminessTTF Nerd Font Mono'),
+  font_size = 17,
+  window_background_opacity = 0.75,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  }
+}
