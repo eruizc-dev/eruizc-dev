616e38bac3e5682eea3931a4e3874f9ddcee539c
Emiliano Ruiz Carletti <emiliano.carletti@mercadolibre.com>
2022-03-18 10:58:08 -0300

    Disable treesitter indentation

--- dotfiles/xdg_config_home/nvim/lua/eruizc/treesitter.lua
+++ dotfiles/xdg_config_home/nvim/lua/eruizc/treesitter.lua
@@ -4,9 +4,5 @@ ts_configs.setup {
   ensure_installed = "maintained",
   highlight = {
     enable = true
-  },
-  indent = {
-    enable = true,
-    disable = { "java", "javascriptreact", "typescriptreact" }
   }
 }
