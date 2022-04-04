8c117b902124dc051de2ef1292cc40fa3d949bc2
Emiliano Ruiz Carletti <emiliano.carletti@mercadolibre.com>
2022-04-04 11:53:42 -0300

    Comment

--- dotfiles/xdg_config_home/wezterm/wezterm.lua
+++ dotfiles/xdg_config_home/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require('wezterm')
 return {
   color_scheme = 'Brogrammer',
   font = wezterm.font('TerminessTTF Nerd Font Mono'),
-  font_size = 17,
+  font_size = 17, -- TODO: Increase font size in mac
   window_background_opacity = 0.75,
   check_for_updates = false,
   window_padding = {
