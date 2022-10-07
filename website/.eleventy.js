b0014f88dc95fd58c150ea0d97405d08a3e00ae3
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-10-07 19:25:53 -0300

    Add humans.txt

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -11,6 +11,7 @@ module.exports = function(config) {
   setupStaticDirectory('./src/resources', config);
   setupStaticDirectory('./src/styles', config);
   setupStaticFile('./src/favicon.png', config);
+  setupStaticFile('./src/humans.txt', config);
 
   return {
     dir: {
