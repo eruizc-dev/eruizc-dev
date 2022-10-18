57522e39ddaff3870a1562c8e7feaa332038209c
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-10-17 21:38:07 -0300

    change font size

--- dotfiles/xdg_config_home/wezterm/wezterm.lua
+++ dotfiles/xdg_config_home/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ return {
   color_scheme = 'Dracula',
   font = wezterm.font('TerminessTTF Nerd Font Mono'),
   hide_tab_bar_if_only_one_tab = true,
-  font_size = 16, -- TODO: Increase font size in mac
+  font_size = 15, -- TODO: Increase font size in mac
   window_background_opacity = 0.69,
   check_for_updates = false,
   window_padding = {
