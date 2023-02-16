134b94c7b71486a5f561ca64d2f75b55daf12393
Emiliano Ruiz Carletti <contact@eruizc.dev>
2023-02-16 13:04:17 -0300

    Update statusline

--- dotfiles/xdg_config_home/nvim/plugin/statusline.lua
+++ dotfiles/xdg_config_home/nvim/plugin/statusline.lua
@@ -13,14 +13,14 @@ require('lualine').setup {
     always_divide_middle = true,
     globalstatus = true,
     refresh = {
-      statusline = 250,
+      statusline = 1000,
       tabline = 1000,
       winbar = 1000,
     }
   },
   sections = {
     lualine_a = { 'branch' },
-    lualine_b = { 'diff' },
+    lualine_b = {},
     lualine_c = {
       { 'filetype', icon_only = true },
       { 'filename', path = 1, symbols = { modified = '', readonly = '', unnamed = '', newfile = '' } }
@@ -28,27 +28,20 @@ require('lualine').setup {
     lualine_x = {{
       'diagnostics',
       always_visible = true,
-      symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
-      sources = { 'nvim_diagnostic' },
-      sections = { 'hint', 'info', 'warn', 'error' }
+      --symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+      sources = { 'nvim_workspace_diagnostic' },
+      sections = { 'info', 'warn', 'error' }
     }},
     lualine_y = {{
       'diagnostics',
       always_visible = true,
       symbols = { error = ' ', warn = ' ' },
-      sources = { 'nvim_workspace_diagnostic' },
+      sources = { 'nvim_diagnostic' },
       sections = { 'warn', 'error' }
     }},
     lualine_z = { 'encoding', 'fileformat' },
   },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {}
-  },
+  inactive_sections = {},
   tabline = {},
   winbar = {},
   inactive_winbar = {},
