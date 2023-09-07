42fbb3477108dc574c90af41803c5e759626643e
Emiliano Ruiz Carletti <contact@eruizc.dev>
2023-09-07 20:55:24 -0300

    Setup golang debug

--- dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/go.lua
+++ dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/go.lua
@@ -2,8 +2,12 @@ return {
 	{
 		'olexsmir/gopher.nvim',
 		ft = 'go',
-		dependencies = { 'mfussenegger/nvim-dap' },
+		dependencies = {
+			'mfussenegger/nvim-dap',
+			{ 'williamboman/mason.nvim', opts = {} }
+		},
 		config = function (_, opts)
+			require'eruizc-dev.utils.mason'.ensure_installed({ 'gopls', 'delve' })
 			require'gopher.dap'.setup(opts)
 		end
 	}
