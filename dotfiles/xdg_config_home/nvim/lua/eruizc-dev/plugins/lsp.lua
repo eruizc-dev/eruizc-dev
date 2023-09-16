aa68e3c96b9d6d0493326e540a934524b50248f9
Emiliano Ruiz Carletti <contact@eruizc.dev>
2023-09-15 22:26:17 -0300

    Move to completion.lua

--- dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/lsp.lua
+++ dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/lsp.lua
@@ -1,5 +1,17 @@
 return {
 	{
+		'zbirenbaum/copilot.lua',
+		cmd = 'Copilot',
+		build = ':Copilot auth',
+		opts = {
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+		},
+		config = function()
+			require('copilot').setup({})
+		end,
+	},
+	{
 		'neovim/nvim-lspconfig',
 		cmd = 'LspInfo',
 		event = { 'BufReadPre', 'BufNewFile' },
@@ -54,116 +66,6 @@ return {
 				require('lspconfig')[server].setup(final_opts)
 			end
 			vim.diagnostic.config(opts) -- Must be after setup
-		end,
-	},
-	{
-		'hrsh7th/nvim-cmp',
-		event = 'InsertEnter',
-		version = false, -- Last version is too old
-		dependencies = {
-			-- Sources
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-nvim-lua',
-			--'kristijanhusak/vim-dadbod-completion',
-			-- Style points
-			'onsails/lspkind-nvim',
-			-- Snippets
-			{
-				'saadparwaiz1/cmp_luasnip',
-				dependencies = {
-					{
-						'L3MON4D3/LuaSnip',
-						dependencies = {
-							{
-								'rafamadriz/friendly-snippets',
-								config = function ()
-									require'luasnip.loaders.from_vscode'.lazy_load()
-								end
-							}
-						},
-					}
-				}
-			},
-			-- Copilot
-			{
-				'zbirenbaum/copilot-cmp',
-				dependencies = 'zbirenbaum/copilot.lua',
-				config = function ()
-					require('copilot_cmp').setup()
-				end
-			}
-		},
-		opts = function(_, opts)
-			local cmp = require'cmp'
-			vim.api.nvim_set_hl(0, 'CmpGhostText', { link = 'Comment', default = true })
-			return {
-				preselect = cmp.PreselectMode.None,
-				completion = {
-					completeopt = 'menu,menuone,noinsert,noselect',
-				},
-				snippet = {
-					expand = function(args)
-						require'luasnip'.lsp_expand(args.body)
-					end,
-				},
-				mapping = {
-					['<C-c>'] = require'cmp'.mapping.abort(),
-					['<CR>'] = require'cmp'.mapping.confirm(),
-					['<C-j>'] = require'cmp'.mapping.select_next_item({ behavior = require'cmp'.SelectBehavior.Select }),
-					['<C-k>'] = require'cmp'.mapping.select_prev_item({ behavior = require'cmp'.SelectBehavior.Select }),
-					['<C-h>'] = require'cmp'.mapping(function(fallback)
-						if require'luasnip'.jumpable(-1) then
-							require'luasnip'.jump(-1)
-						else
-							require'cmp'.mapping.scroll_docs(-4)
-						end
-					end, { 'i', 's' }),
-					['<C-l>'] = require'cmp'.mapping(function(fallback)
-						if require'luasnip'.expand_or_jumpable() then
-							require'luasnip'.expand_or_jump()
-						else
-							require'cmp'.mapping.scroll_docs(4)
-						end
-					end, { 'i', 's' }),
-				},
-				sources = {
-					{
-						name = 'nvim_lsp',
-						entry_filter = function(entry, context)
-							local Kind = require'cmp.types.lsp'.CompletionItemKind
-							local kind = entry:get_kind()
-							local char_before_cursor = string.sub(context.cursor_line, context.cursor.col - 1, context.cursor.col - 1)
-							if char_before_cursor == '.' then
-								return kind == Kind.Method or kind == Kind.Field
-							elseif string.match(context.cursor_line, '^%s*%w*$') then
-								return kind == Kind.Function or kind == Kind.Variable
-							else
-								return true
-							end
-						end
-					},
-					{ name = 'nvim_lua' },
-					{ name = 'buffer' },
-					{ name = 'path' },
-					{ name = 'luasnip' },
-					{ name = 'copilot' },
-					--{ name = 'vim-dadbod-completion' },
-				},
-				formatting = {
-					format = require'lspkind'.cmp_format({
-						mode = 'symbol_text',
-						maxwidth = 50,
-						symbol_map = { Copilot = '' }
-					})
-				},
-				experimental = {
-					ghost_text = {
-						hl_group = 'CmpGhostText',
-					},
-				},
-			}
 		end,
 	},
 }
