32503da453a8b130a03ac28f8b4392c57382561d
Emiliano Ruiz Carletti <contact@eruizc.dev>
2023-11-08 23:51:20 -0300

    Add markdown preview

--- dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/helpers.lua
+++ dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/helpers.lua
@@ -1,5 +1,15 @@
 return {
 	{
+		'iamcco/markdown-preview.nvim',
+		ft = 'markdown',
+		build = function()
+			vim.fn['mkdp#util#install']()
+		end,
+		init = function()
+			vim.g.mkdp_port = 8080
+		end
+	},
+	{
 		'sudormrfbin/cheatsheet.nvim',
 		cmd = 'Cheatsheet',
 		dependencies = 'nvim-telescope/telescope.nvim',
