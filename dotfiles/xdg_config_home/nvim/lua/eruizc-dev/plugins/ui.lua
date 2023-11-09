b5247b1d6a890ea72a01d4396eab5d2dbc9d9797
Emiliano Ruiz Carletti <contact@eruizc.dev>
2023-11-09 09:45:31 -0300

    Update dadbod

--- dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/ui.lua
+++ dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/ui.lua
@@ -1,16 +1,8 @@
 return {
 	{
 		'kristijanhusak/vim-dadbod-ui',
-		dependencies = {
-			'tpope/vim-dadbod',
-			{ 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' } }
-		},
-		cmd = {
-			'DBUI',
-			'DBUIToggle',
-			'DBUIAddConnection',
-			'DBUIFindBuffer',
-		},
+		dependencies = 'tpope/vim-dadbod',
+		cmd = { 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DBUIFindBuffer', },
 		init = function()
 			vim.g.db_ui_use_nerd_fonts = 1
 			vim.g.db_ui_table_helpers = {
