5aabfec16341b7919c49ab70d120c385c08783df
Emiliano Ruiz Carletti <contact@eruizc.dev>
2024-02-02 14:16:49 -0300

    Move nvim-jdtls to lsp.lua

--- dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/lsp.lua
+++ dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/lsp.lua
@@ -14,10 +14,23 @@ return {
 	{
 		'mfussenegger/nvim-lint',
 		config = function()
+			require'eruizc-dev.utils.mason'.ensure_installed({ 'checkstyle' })
 			require('lint').linters_by_ft = {
 			  java = { 'checkstyle' }
 			}
 		end,
+	},
+	{
+		'mfussenegger/nvim-jdtls',
+		ft = 'java',
+		dependencies = {
+			'mfussenegger/nvim-dap',
+			'hrsh7th/cmp-nvim-lsp',
+			'williamboman/mason.nvim',
+		},
+		config = function ()
+			require'eruizc-dev.utils.mason'.ensure_installed({ 'jdtls', 'java-test', 'java-debug-adapter' })
+		end
 	},
 	{
 		'neovim/nvim-lspconfig',
