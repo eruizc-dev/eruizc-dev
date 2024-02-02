004f6a8073888121139bafb015a6da615c92ee49
Emiliano Ruiz Carletti <contact@eruizc.dev>
2024-02-02 14:16:54 -0300

    Trigger lint

--- dotfiles/xdg_config_home/nvim/ftplugin/java.lua
+++ dotfiles/xdg_config_home/nvim/ftplugin/java.lua
@@ -4,6 +4,12 @@ end
 
 vim.cmd[[set tabstop=4]]
 
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    require'lint'.try_lint()
+  end,
+})
+
 require'jdtls'.start_or_attach({
 	cmd = {
 		'jdtls',
