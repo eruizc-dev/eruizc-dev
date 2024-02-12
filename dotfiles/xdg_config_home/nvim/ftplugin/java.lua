b26433750fc83e0aa6562eed223bbc8f9056c481
Emiliano Ruiz Carletti <contact@eruizc.dev>
2024-02-11 21:52:03 -0300

    Simplify jdtls config

--- dotfiles/xdg_config_home/nvim/ftplugin/java.lua
+++ dotfiles/xdg_config_home/nvim/ftplugin/java.lua
@@ -1,21 +1,12 @@
-local function path(package_name)
-	return require'mason-registry'.get_package(package_name):get_install_path()
-end
-
 vim.cmd[[set tabstop=4]]
 
 require'jdtls'.start_or_attach({
-	cmd = {
-		'jdtls',
-		'-data',
-		vim.fn.expand('$HOME') .. '/.cache/jdtls-workspace',
-		'--jvm-arg=-javaagent:' .. path'jdtls' .. '/lombok.jar',
-	},
+    cmd = {
+        vim.fn.expand'$HOME/.local/share/nvim/mason/bin/jdtls',
+        ('--jvm-arg=-javaagent:%s'):format(vim.fn.expand'$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar')
+     },
 	init_options = {
-		bundles = require'eruizc-dev.utils.list'.join(
-			vim.fn.glob(path'java-debug-adapter' .. '/extension/server/com.microsoft.java.debug.plugin-*.jar', 1) ,
-			vim.split(vim.fn.glob(path'java-test' .. '/extension/server/*.jar', 1), "\n")
-		)
+		bundles = vim.fn.glob(vim.split('$HOME/.local/share/nvim/mason/packages/java-*/extension/server/*.jar', '\n'), 1),
 	},
 	settings = {
 		java = {
@@ -43,9 +34,6 @@ require'jdtls'.start_or_attach({
 	},
 	capabilities = require'cmp_nvim_lsp'.default_capabilities(),
 	on_attach = function(_client)
-		require'jdtls.setup'.add_commands()
 		require'jdtls'.update_project_config()
-		require'jdtls'.setup_dap({ hotcodereplace = 'auto' })
 	end
 })
-
