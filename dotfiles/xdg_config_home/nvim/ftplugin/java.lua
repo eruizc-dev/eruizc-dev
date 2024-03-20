fa641889ca52f6d65884c15546ee657f6b7ef515
Emiliano Ruiz Carletti <contact@eruizc.dev>
2024-03-20 15:52:02 -0300

    Fix jdtls config

--- dotfiles/xdg_config_home/nvim/ftplugin/java.lua
+++ dotfiles/xdg_config_home/nvim/ftplugin/java.lua
@@ -1,12 +1,12 @@
 vim.cmd[[set tabstop=4]]
 
 require'jdtls'.start_or_attach({
-    cmd = {
-        vim.fn.expand'$HOME/.local/share/nvim/mason/bin/jdtls',
-        ('--jvm-arg=-javaagent:%s'):format(vim.fn.expand'$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar')
-     },
+	cmd = {
+		vim.fn.expand'$HOME/.local/share/nvim/mason/bin/jdtls',
+		('--jvm-arg=-javaagent:%s'):format(vim.fn.expand'$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar')
+	},
 	init_options = {
-		bundles = vim.fn.glob(vim.split('$HOME/.local/share/nvim/mason/packages/java-*/extension/server/*.jar', '\n'), 1),
+		bundles = vim.split(vim.fn.glob('$HOME/.local/share/nvim/mason/packages/java-*/extension/server/*.jar', 1), '\n'),
 	},
 	settings = {
 		java = {
