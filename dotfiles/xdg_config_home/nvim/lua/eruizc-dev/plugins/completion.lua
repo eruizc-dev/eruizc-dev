ac654d8fda3bc8f83b55bf20746d59925dd45a6b
Emiliano Ruiz Carletti <contact@eruizc.dev>
2024-03-20 15:51:49 -0300

    Remove copilot

--- dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/completion.lua
+++ dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/completion.lua
@@ -26,13 +26,6 @@ return {
 				}
 			},
 			{
-				'zbirenbaum/copilot-cmp',
-				dependencies = 'zbirenbaum/copilot.lua',
-				config = function ()
-					require('copilot_cmp').setup()
-				end
-			},
-			{
 				'kristijanhusak/vim-dadbod-completion',
 				ft = { 'sql', 'mysql', 'plsql' }
 			},
