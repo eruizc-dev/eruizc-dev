59adbdbe47352cce59c35f9a64cf5ddb86f79fae
Emiliano Ruiz Carletti <contact@eruizc.dev>
2024-06-20 19:39:01 -0300

    Fix treesitter installation

--- dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/color.lua
+++ dotfiles/xdg_config_home/nvim/lua/eruizc-dev/plugins/color.lua
@@ -14,19 +14,40 @@ return {
 		version = false, -- last release is way too old and doesn't work on Windows
 		build = ':TSUpdate',
 		event = { 'BufReadPost', 'BufNewFile' },
-		cmd = { 'TSUpdateSync' },
+		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 		dependencies = 'williamboman/mason.nvim',
+		init = function(plugin)
+			-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+			-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+			-- no longer trigger the **nvim-treesitter** module to be loaded in time.
+			-- Luckily, the only things that those plugins need are the custom queries, which we make available
+			-- during startup.
+			require("lazy.core.loader").add_to_rtp(plugin)
+			require("nvim-treesitter.query_predicates")
+		end,
+		---@type TSConfig
+		---@diagnostic disable-next-line: missing-fields
 		opts = {
-			ensure_installed = { 'lua', 'java', 'ruby', 'go' },
+			sync_install = false,
+			auto_install = true,
+			ensure_installed = {
+				'lua',
+				'java',
+				'ruby',
+				'go', 'templ',
+			},
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = false,
 			},
-			indent = { enable = true },
-			auto_install = true,
+			indent = {
+				enable = true,
+			},
 		},
-		config = function(_, _opts)
+		---@param opts TSConfig
+		config = function(_, opts)
 			require'eruizc-dev.utils.mason'.ensure_installed({ 'tree-sitter-cli' })
+			require("nvim-treesitter.configs").setup(opts)
 		end
 	}
 }
