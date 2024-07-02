242cc0431c40e57e1e3dbc5306f3610585af0862
Emiliano Ruiz Carletti <contact@eruizc.dev>
2024-07-02 18:20:20 -0300

    Include lombok again

--- dotfiles/xdg_config_home/nvim/ftplugin/java.lua
+++ dotfiles/xdg_config_home/nvim/ftplugin/java.lua
@@ -1,7 +1,10 @@
 vim.cmd[[set tabstop=4]]
 
 require'jdtls'.start_or_attach({
-	cmd = { vim.fn.expand'$HOME/.local/share/nvim/mason/bin/jdtls' },
+	cmd = {
+		vim.fn.expand'$HOME/.local/share/nvim/mason/bin/jdtls',
+		('--jvm-arg=-javaagent:%s'):format(vim.fn.expand'$HOME/.local/share/nvim/mason/packages/jdtls/lombok.jar')
+	},
 	init_options = {
 		bundles = vim.split(vim.fn.glob('$HOME/.local/share/nvim/mason/packages/java-*/extension/server/*.jar', true), '\n'),
 	},
