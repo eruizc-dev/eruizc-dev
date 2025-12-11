6a7223e44de621122bafda62ad23c0ca29ff5f7b
Emiliano Ruiz Carletti <contact@eruizc.dev>
2025-12-11 17:31:34 -0300

    Configurar jdtls para usarse con nix (variable de entorno)

--- dotfiles/.config/nvim/ftplugin/java.lua
+++ dotfiles/.config/nvim/ftplugin/java.lua
@@ -7,11 +7,21 @@ if not jdtls_enabled then
 	return
 end
 
+local bundles = { vim.fn.glob('$JAVA_DEBUG/share/vscode/extensions/vscjava.vscode-java-debug/server/com.microsoft.java.debug.plugin-*.jar', 1) }
+local java_test_bundles = vim.split(vim.fn.glob('$JAVA_TEST/share/vscode/extensions/vscjava.vscode-java-test/server/*.jar', 1), "\n")
+local excluded = { 'com.microsoft.java.test.runner-jar-with-dependencies.jar', 'jacocoagent.jar' }
+for _, java_test_jar in ipairs(java_test_bundles) do
+  local fname = vim.fn.fnamemodify(java_test_jar, ":t")
+  if not vim.tbl_contains(excluded, fname) then
+    table.insert(bundles, java_test_jar)
+  end
+end
+
 jdtls.start_or_attach({
 	cmd = { 'jdtls' },
 	root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
 	init_options = {
-		bundles = vim.split(vim.fn.glob('$HOME/.local/share/nvim/mason/packages/java-*/extension/server/*.jar', true), '\n'),
+		bundles = bundles
 	},
 	settings = {
 		java = {
